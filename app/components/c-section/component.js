04a2ed89000ed41a62fc3ef69edb598d7dbf0663
Jonas Büchel <jonas.buechel@gmail.com>
2016-10-11 23:03:10 +0200

    Add option for defining a background image at c-section and highlight the component in red, if a background is set.

--- app/components/c-section/component.js
+++ app/components/c-section/component.js
@@ -4,5 +4,7 @@ export default Ember.Component.extend({
   tagName: 'section',
   classNames: ['c-section', 'container'],
   classNameBindings: ['isInProgress:c-section--in-progress'],
-  isInProgress: false
+  isInProgress: false,
+  backgroundimage: false,
+  attributeBindings: ['backgroundimage']
 });
