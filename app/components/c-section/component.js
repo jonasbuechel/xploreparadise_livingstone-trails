c82698345eca310afb94062e2c3beddcf4c25bbd
Jonas Büchel <jonas.buechel@gmail.com>
2016-10-12 06:52:28 +0200

    Add option to set a background-image and some positioning options for a c-section

--- app/components/c-section/component.js
+++ app/components/c-section/component.js
@@ -2,9 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'section',
-  classNames: ['c-section', 'container'],
-  classNameBindings: ['isInProgress:c-section--in-progress'],
+  classNames: ['c-section'],
+  isConstrained: false,
   isInProgress: false,
   backgroundimage: false,
-  attributeBindings: ['backgroundimage']
+  backgroundStyleCode: Ember.computed('backgroundimage', function(){
+    let backgroundimage = this.get('backgroundimage');
+    if(backgroundimage){
+      return `background-image:url("${backgroundimage}")`;
+    }
+  }),
+  classNameBindings: ['isInProgress:c-section--in-progress', 'isConstrained:container'],
+  attributeBindings: ['backgroundStyleCode:style']
 });
