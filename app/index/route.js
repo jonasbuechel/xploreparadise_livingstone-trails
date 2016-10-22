6ca477f078d8b8a3590c8b3ee89de9488f7217b2
Jonas Büchel <jonas.buechel@gmail.com>
2016-10-22 19:16:07 +0200

    Optimize data-handling for video-teaser integration

--- app/index/route.js
+++ app/index/route.js
@@ -37,6 +37,33 @@ export default Ember.Route.extend({
         p2: 'Follow Xploreparadise on Instagram and Facebook and discover the story behind the images, what happened each day of my adventure and learn some interesting facts about David Livingstone and the Kalahari wildlife.',
         p3: 'Share the pictures and videos with your friends and become a part of this incredible adventure.'
       },
+      videoTeasers: [
+        {
+          youtubeId: '6vqFleiDX1A',
+          backgroundimage: 'img_video_cheetahs.jpg',
+          title: 'Cheethas',
+          subtitle: 'With the meercats'
+        },
+        {
+          youtubeId: 'K_MbZ2-UcJM',
+          backgroundimage: 'img_video_meerkats.jpg',
+          title: 'The guardian',
+          subtitle: 'With the meercats'
+        },
+        {
+          youtubeId: 'GWJ3-gBWFmU',
+          backgroundimage: 'img_video_elephants.jpg',
+          title: 'At the water hole',
+          subtitle: 'Cheethas'
+        },
+        {
+          youtubeId: 'OLsjeRDvjnk',
+          backgroundimage: 'img_video_springboks.jpg',
+          title: 'At the water hole',
+          subtitle: 'Cheethas'
+        }
+        
+      ],
       about: {
         h2: 'ABOUT THE ORGINATOR AND ADVENTURER',
         h3: 'The person behind Xploreparadise',
