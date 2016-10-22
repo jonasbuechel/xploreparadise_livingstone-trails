1d77869fc8d7e32dd810df0ef181621ec9923a86
Jonas Büchel <jonas.buechel@gmail.com>
2016-10-22 18:58:24 +0200

    Add modal-video component and its configuration to the index page with a styling addition

--- app/index/controller.js
+++ app/index/controller.js
@@ -4,5 +4,13 @@ export default Ember.Controller.extend({
   youtubeIntroPlayerVars: {
     autoplay: 1,
     rel: 0
-  }
+  },
+  showVideoModal: false,
+  videoId: undefined,
+  actions: {
+    openYouTubeModal(youtubeId){
+      this.set('videoId', youtubeId)
+      this.set('showVideoModal', true);
+    }
+  },
 });
