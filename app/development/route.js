e1e3178d1d32b5f32ffbee16341c990558316b93
Jonas Büchel <jonas.buechel@gmail.com>
2016-10-23 09:30:51 +0200

    Integrate the about section at development

--- app/development/route.js
+++ app/development/route.js
@@ -1,13 +1,14 @@
 import Ember from 'ember';
-import mixinRouteMainGallery from '../mixins/route-main-gallery';
 
-
-export default Ember.Route.extend(mixinRouteMainGallery,{  
+export default Ember.Route.extend({  
   model(){
     
     return {
-      mainGallery: {
-        items: this.get('mainGalleryItems')
+     about: {
+        h2: 'ABOUT THE ORGINATOR AND ADVENTURER',
+        h3: 'The person behind Xploreparadise',
+        p1: 'As a young boy, Christoph Tänzer began his deep love of exploration, adventure and nature, dreaming about foreign countries, intriguing places and different cultures. More than 30 years later, these dreams came true for the German creative director, photographer and filmmaker. His passion developed into an idea: Winning people’s understanding through photography and film to protect our planet.',
+        p2: '“I tell stories by capturing moments that show the beauty of our planet and the variety of its wildlife, people and cultures. If people understand that some of these moments, places, animals or people will be rare or gone in a couple of years, it’s a start. If they decide to change something, it will make a difference and my mission will have been successful.”'
       }
     }
   }
