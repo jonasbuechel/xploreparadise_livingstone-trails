0f206da772e60b93a966920545f223ff890435f3
Jonas Büchel <jonas.buechel@gmail.com>
2016-10-25 22:53:20 +0200

    Remove equipment title

--- app/index/route.js
+++ app/index/route.js
@@ -80,7 +80,6 @@ export default Ember.Route.extend(mixinRouteMainGallery,{
         p1: 'LUCKY GARENAMOTSE & STAFF OF <a title="GIFTS OF THE KALAHARI" target="_blank" href="http://www.giftsofthekalahari.com">GIFTS OF THE KALAHARI</a>, BOTSWANA I GLEN STEPHENS AND TSHEPI GARENAMOTSE OF <a title="LIQUID GIRAFFE" target="_blank" href="http://liquidgiraffe.com">LIQUID GIRAFFE</a> I GAIL AND DOUG & STAFF OF <a title="THE CHUNDUKWA RIVER LODGE" target="_blank" href="http://www.chundukwariverlodge.com">THE CHUNDUKWA RIVER LODGE</a>, ZAMBIA I THE MINISTRY OF ENVIROMENT, WILDLIFE AND TOURISM OF BOTSWANA I THE CHOBE NATIONAL PARK, BOTSWANA I THE CKGR, BOTSWANA I THE MAKADIKADI PANS PARK, BOTSWANA I JANINE AVERY & <a title="AFRICA GEOGRAPHIC" target="_blank" href="http://africageographic.com/">AFRICA GEOGRAPHIC</a> I CHRISTOPH HIRSCHBÜHL & JONAS BÜCHEL I MY FRIENDS & FAMILY'
       },
       equipment: {
-        h2: 'EQUIPMENT',
         p1: 'Christoph Tänzer operates with gear from'
       },
       copyright: {
