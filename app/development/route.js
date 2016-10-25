4a5c7cdf17167eb4408a0a86f8e2778e97399e7e
Jonas Büchel <jonas.buechel@gmail.com>
2016-10-25 22:53:48 +0200

    Add a responsive configuration for the slick carousel to the development route

--- app/development/route.js
+++ app/development/route.js
@@ -3,10 +3,31 @@ import Ember from 'ember';
 export default Ember.Route.extend({  
   model(){
     return {
-      thanks: {
-        h2: 'SPECIAL THANKS',
-        p1: 'LUCKY GARENAMOTSE & STAFF OF <a title="GIFTS OF THE KALAHARI" target="_blank" href="http://www.giftsofthekalahari.com">GIFTS OF THE KALAHARI</a>, BOTSWANA I GLEN STEPHENS AND TSHEPI GARENAMOTSE OF <a title="LIQUID GIRAFFE" target="_blank" href="http://liquidgiraffe.com">LIQUID GIRAFFE</a> I GAIL AND DOUG & STAFF OF <a title="THE CHUNDUKWA RIVER LODGE" target="_blank" href="http://www.chundukwariverlodge.com">THE CHUNDUKWA RIVER LODGE</a>, ZAMBIA I THE MINISTRY OF ENVIROMENT, WILDLIFE AND TOURISM OF BOTSWANA I THE CHOBE NATIONAL PARK, BOTSWANA I THE CKGR, BOTSWANA I THE MAKADIKADI PANS PARK, BOTSWANA I JANINE AVERY & <a title="AFRICA GEOGRAPHIC" target="_blank" href="http://africageographic.com/">AFRICA GEOGRAPHIC</a> I CHRISTOPH HIRSCHBÜHL & JONAS BÜCHEL I MY FRIENDS & FAMILY'
+      equipment: {
+        p1: 'Christoph Tänzer operates with gear from',
+        responsiveSettings: [
+          {
+            breakpoint:1200,
+            settings:{
+              slidesToShow:4,
+              infinite:true
+            }
+          },
+          {
+            breakpoint:992,
+            settings:{
+              slidesToShow:3,
+              infinite:true
+            }
+          },
+          {
+            breakpoint:768,
+            settings:{
+              slidesToShow:2,
+              dots:true}
+          }
+        ]
       }
-    }
+    };
   }
 });
