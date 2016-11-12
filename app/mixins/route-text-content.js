0a10db4eb52e3676b881280058718194861f3fd3
Jonas Büchel <jonas.buechel@gmail.com>
2016-11-12 06:40:49 +0100

    Separate content 'About' to different areas

--- app/mixins/route-text-content.js
+++ app/mixins/route-text-content.js
@@ -89,7 +89,9 @@ export default Ember.Mixin.create({
       text: [
         {
           content:'As a young boy, Christoph Tänzer began his deep love of exploration, adventure and nature, dreaming about foreign countries, intriguing places and different cultures. More than 30 years later, these dreams came true for the German creative director, photographer and filmmaker. His passion developed into an idea: Winning people’s understanding through photography and film to protect our planet.'
-        },
+        }
+      ],
+      text2: [
         {
           content:'“I tell stories by capturing moments that show the beauty of our planet and the variety of its wildlife, people and cultures. If people understand that some of these moments, places, animals or people will be rare or gone in a couple of years, it’s a start. If they decide to change something, it will make a difference and my mission will have been successful.”'
         }
