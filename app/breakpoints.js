8b80031941947057ff2002a90424c3f3888fc722
Jonas Büchel <jonas.buechel@gmail.com>
2016-12-03 13:07:43 +0100

    Install ember-responsive and configure it for Bootstrap 3 defaults

---
+++ app/breakpoints.js
@@ -0,0 +1,6 @@
+export default {
+  xs: '(max-width: 768px)',
+  sm: '(min-width: 769px) and (max-width: 992px)',
+  md: '(min-width: 993px) and (max-width: 1200px)',
+  lg: '(min-width: 1201px)'
+};
