f89e77e2362dde8ef8061d1d21ffc7c782005f06
Jonas Büchel <jonas.buechel@gmail.com>
2016-12-04 19:30:23 +0100

    Install stellar.js and its dependency jquery-migrate

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -26,6 +26,9 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+  
+  app.import("bower_components/jquery-migrate/index.js");
+  app.import("bower_components/jquery.stellar/jquery.stellar.min.js");
 
   return app.toTree();
 };
