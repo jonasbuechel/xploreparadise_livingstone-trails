408d845388da91f28ea0791b99c3728014177391
Jonas Büchel <jonas.buechel@gmail.com>
2016-12-07 22:03:22 +0100

    Add styling variants for different parallax speeds

--- app/components/c-section/component.js
+++ app/components/c-section/component.js
@@ -8,6 +8,10 @@ export default Ember.Component.extend(mixinComponentBackgroundImage,{
   isConstrainedInside: false,
   isInProgress: false,
   parallaxBackgroundRatio: false,
-  classNameBindings: ['isInProgress:c-section--in-progress', 'isConstrained:container'],
+  isParallax50: false,
+  isParallax75: false,
+  isParallax125: false,
+  isParallax150: false,
+  classNameBindings: ['isInProgress:c-section--in-progress', 'isConstrained:container', 'isParallax125:c-section--background-parallax-125', 'isParallax150:c-section--background-parallax-150', 'isParallax75:c-section--background-parallax-75', 'isParallax50:c-section--background-parallax-50'],
   attributeBindings: ['backgroundStyleCode:style','parallaxBackgroundRatio:data-stellar-background-ratio']
 });
