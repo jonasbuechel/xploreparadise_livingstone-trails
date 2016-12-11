3af56af4133f5b961ce76886c7d27ad93604ccac
Jonas Büchel <jonas.buechel@gmail.com>
2016-12-11 13:59:59 +0100

    Add attribute to background-image mixin, which returns the path only

--- app/mixins/component-background-image.js
+++ app/mixins/component-background-image.js
@@ -4,8 +4,18 @@ export default Ember.Mixin.create({
   backgroundimage: false,
   backgroundStyleCode: Ember.computed('backgroundimage', function(){
     let backgroundimage = this.get('backgroundimage');
-    if(backgroundimage){
+    const isParallax = this.get('isParallax');
+    
+    if(backgroundimage && isParallax !== true){
       return `background-image:url("/assets/${backgroundimage}")`;
+    } else {
+      return false;
+    }
+  }),
+  backgroundImagePath: Ember.computed('backgroundimage', function(){
+    let backgroundimage = this.get('backgroundimage');
+    if(backgroundimage){
+      return `/assets/${backgroundimage}`;
     }
   })
 });
