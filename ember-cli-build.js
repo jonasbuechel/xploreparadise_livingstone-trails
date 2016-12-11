d6ffeb52fa1812a2a2a4117df847f299f895faca
Jonas Büchel <jonas.buechel@gmail.com>
2016-12-11 14:45:36 +0100

    Remove unused stellar plugin and it's jquery dependencies

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -27,8 +27,6 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
   
-  app.import("bower_components/jquery-migrate/index.js");
-  app.import("bower_components/jquery.stellar/jquery.stellar.min.js");
   app.import("bower_components/parallax.js/parallax.min.js");
 
   return app.toTree();
