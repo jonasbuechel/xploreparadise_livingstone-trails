44b4d2d406dea3df37879c967346b3bce9ff191a
Jonas Büchel <jonas.buechel@gmail.com>
2016-12-12 06:32:10 +0100

    Remove unused fork for parallax backgrounds

--- app/mixins/component-background-image.js
+++ app/mixins/component-background-image.js
@@ -4,13 +4,8 @@ export default Ember.Mixin.create({
   backgroundimage: false,
   backgroundStyleCode: Ember.computed('backgroundimage', function(){
     let backgroundimage = this.get('backgroundimage');
-    const isParallax = this.get('isParallax');
     
-    if(backgroundimage && isParallax !== true){
-      return `background-image:url("/assets/${backgroundimage}")`;
-    } else {
-      return false;
-    }
+    return `background-image:url("/assets/${backgroundimage}")`;
   }),
   backgroundImagePath: Ember.computed('backgroundimage', function(){
     let backgroundimage = this.get('backgroundimage');
